c9ba1b5c216b6110f9d67f3a16f55ff12e4e9023
chrisfoose <47850167+chrisfoose@users.noreply.github.com>
2020-05-11 15:38:29 -1000

    Create randomColor.py

---
+++ randomColor.py
@@ -0,0 +1,7 @@
+# Random Color Generator
+# Chooses random color from out of four choices
+# Christopher Foose
+
+import random
+colorList = ["Red", "Blue", "Green", "Yellow"]
+print("Your color is: ", random.choice(colorList))
